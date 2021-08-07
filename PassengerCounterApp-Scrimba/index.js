b13e46c14a02d895ae9955d0bcd5b9112a553560
Tyson Romero <romennoodles23@yahoo.com>
2021-08-07 12:57:27 -0600

    app works and can save # of people that go on each train, and then reset to 0

--- PassengerCounterApp-Scrimba/index.js
+++ PassengerCounterApp-Scrimba/index.js
@@ -2,17 +2,18 @@
 
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
-
 let count = 0
 
 function increment() {
     count += 1;
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function save() {
     let countStr = count + " - ";
     saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
 
 
