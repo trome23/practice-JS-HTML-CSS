f990738f017f7b3effba52027cb0a2d5a5e1ce45
Tyson Romero <romennoodles23@yahoo.com>
2021-08-07 12:49:30 -0600

    added saved entries feature

--- PassengerCounterApp-Scrimba/index.js
+++ PassengerCounterApp-Scrimba/index.js
@@ -1,16 +1,20 @@
 // // document.getElementById("count-el").innerText = 5
 
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 
-console.log(countEl)
-
-let count = 1
+let count = 0
 
 function increment() {
-    count = count + 1;
+    count += 1;
     countEl.innerText = count
-    console.log(count)
 }
+
+function save() {
+    let countStr = count + " - ";
+    saveEl.textContent += countStr
+}
+
 
 
 
